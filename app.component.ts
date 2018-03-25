5b8735e53108c9d20c0f238ada4c445371fc39d1
Meir Yeheskel <meir.yeheskel>
2018-03-26 01:46:35 +0300

    Initial commit

---
+++ app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+     arr:Array<number>=[1,2,3,4,5,6,7,8,9];
+     
+}
